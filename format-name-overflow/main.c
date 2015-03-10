4c814b6ca6e6c39da02f9c6255146b173005e52d
Danny Qiu <dqiu55@gmail.com>
2015-03-10 10:04:49 -0400

    Fix the buffer overflow/format string attack problem

--- format-name-overflow/main.c
+++ format-name-overflow/main.c
@@ -1,16 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+char *flag = "stuyctf{0V3rfl0w_format_string_l34k}";
+
+void give_flag(int flag_length, char *flag_to_give, char *message) {
+    printf("Welcome to stuyCTF! Here is your flag: \n"); // Why does this not work??
+    printf("It should be %d characters long.\n", flag_length);
+    printf(message);
+}
 
 int main() {
-    char *FLAG = "Flag: stuyctf{s0_much_format_string_l34k}";
+    char message[96] = "Haha you'll never figure out the flag! :P\n";
+    char name[32];
+    bzero(name, 32);
 
-    char name[64];
     printf("What is your name?\n");
-    fflush(stdout);
-    fgets(name, sizeof(name) * sizeof(short), stdin);
+    fgets(name, sizeof(name) + sizeof(message), stdin); // I account for the size of the message and name arrays, so I should be safe right?
 
-    printf("Hello ");
+    printf("Hello %s\n", name);
+    give_flag(strlen(flag), flag, message);
+
     fflush(stdout);
-    printf(name);
     return 0;
 }
