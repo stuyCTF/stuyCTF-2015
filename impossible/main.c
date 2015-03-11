75ac64bc38a70d114b40cff0b6b9030ed1facb4d
ChesleyTan <ChesleyTan97@gmail.com>
2015-03-10 21:37:16 -0400

    Add source code and solution for impossible

---
+++ impossible/main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[]) {
+    /* This is how the flag is encrypted
+    char flag[34] = "stuyctf{/\\|\\|YT|-|1|\\|G5P0551813}";
+    // Encrypt flag
+    int i;
+    for (i = 0; i < sizeof(flag) - 1; ++i) {
+        flag[i] = ((flag[i] - 12) ^ flag[i+1]);
+        printf("%d\n", flag[i]);
+    }
+    */
+    char flag[34] = {19, 29, 16, 14, 35, 14, 33, 64, 127, 44, 44, 44, 41, 25, 52, 93, 93, 65, 89, 44, 44, 55, 14, 121, 116, 17, 28, 24, 29, 29, 22, 90, 113, 0};
+    char password[34];
+    printf("I would tell you my secret, but I need to know the password!\n");
+    fgets(password, sizeof(password), stdin);
+
+    // Decrypt flag
+    flag[sizeof(flag) - 1] = 0;
+    int i;
+    for (i = sizeof(flag) - 1; i > 0; --i) {
+        flag[i-1] = (flag[i-1] ^ flag[i]) + 12;
+    }
+
+    if (strcmp(password, flag) == 0) {
+        printf("Yay! Here's the flag: %s\n", flag);
+    }
+    else {
+        printf("That's not the password!\n");
+    }
+    return 0;
+}
