fee46c70a7de06872c3ee96a58457f4878ab58aa
Danny Qiu <dqiu55@gmail.com>
2015-03-21 11:44:19 -0400

    Make forms problem solvable

--- Forms/main.php
+++ Forms/main.php
@@ -1,31 +1,30 @@
-<html>
-
+<html>
 <head>
 <title>PHP 1</title>
 </head>
-
 <body>
 
 <?php
 
-flag = "stuyctf{forms_2_easy}"
+$flag = "stuyctf{forms_2_easy}";
 
-a = $_POST["PIN"];
+$a = $_POST["PIN"];
+$show = $_POST["showsource"];
 
-show = $_POST["showsource"];
-
-if(isset($show)) {
-    echo "
-        if($a === -19827747736161128312837161661727773716166727272616149001823847) {
-            echo $flag;
-        } else {
-            echo 'User with provided PIN not found.'; 
-        }
-    "
+if (isset($show)) {
+    echo '<pre>
+$a = $_POST["PIN"];
+$show = $_POST["showsource"];
+if ($a == -19827747736161128312837161661727773716166727272616149001823847) {
+    echo $flag;
+} else {
+    echo "User with provided PIN not found."; 
+}
+    </pre>';
 }
 
-if(isset($a)) {
-    if($a === -19827747736161128312837161661727773716166727272616149001823847) {
+if (isset($a)) {
+    if ($a == -19827747736161128312837161661727773716166727272616149001823847) {
         echo $flag;
     } else {
         echo "User with provided PIN not found.";
@@ -34,5 +33,11 @@ if(isset($a)) {
 
 ?>
 
+<form action="" method="post">
+    PIN:<br>
+    <input type="password" name="PIN" value="">
+    <input type="hidden" name="showsource" value=0>
+    <button type="submit">Enter</button>
+</form>
 </body>
 </html>
