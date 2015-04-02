0cc5d835df37e708830b6ca15afd611f432fa7ad
Danny Qiu <dqiu55@gmail.com>
2015-04-01 23:55:39 -0400

    Modify problem formatter to output problem.json and static folder in server folder

--- generate_problem_format.py
+++ generate_problem_format.py
@@ -1,16 +1,18 @@
-import os, glob, json
+#! /usr/bin/python
+import os, glob, json, shutil, sys
 
 PROBLEM_FILE = "problem.txt"
 HINT_FILE = "hint.txt"
 GRADER_FILE = "grader.py"
-WEIGHTMAP_FILE = "sample-weightmap.json"
+WEIGHTMAP_FILE = "weightmap.json"
 RELEASE_FOLDER = "release"
 
-files = glob.glob("./*")
-folders = []
-for name in files:
-    if not os.path.isfile(name):
-        folders.append(name)
+SERVER_FOLDER = "--stuyctf_server/"
+
+try:
+    shutil.rmtree(SERVER_FOLDER)
+except:
+    pass
 
 template = """
 {{
@@ -25,6 +27,14 @@ template = """
 }}
 """
 
+files = glob.glob("./*")
+folders = []
+for name in files:
+    if not os.path.isfile(name):
+        folders.append(name)
+
+os.mkdir(SERVER_FOLDER)
+
 problems = []
 for folder in folders:
     folder += "/"
@@ -33,12 +43,12 @@ for folder in folders:
     try:
         problem_description = open(folder + PROBLEM_FILE, "r").read()
     except IOError, e:
-        print e
+        print "Looking for problem file...", e
         problem_description = ""
     try:
         problem_hint = open(folder + HINT_FILE, "r").read()
     except IOError, e:
-        print e
+        print "Looking for hint file...", e
         problem_hint = ""
     try:
         weightmaps = json.loads(open(folder + WEIGHTMAP_FILE, "r").read())
@@ -48,7 +58,7 @@ for folder in folders:
         problem_weightmap = "{}"
         problem_threshold = 0
     problem_grader = folder + GRADER_FILE
-    problems.append(template.format(
+    problem = template.format(
         name = problem_name,
         score = problem_score,
         grader = problem_grader,
@@ -56,7 +66,18 @@ for folder in folders:
         hint = problem_hint.replace("\n","<br>"),
         threshold = problem_threshold,
         weightmap = problem_weightmap
-    ))
+    )
+    problems.append(problem)
+    try:
+        os.mkdir(SERVER_FOLDER + problem_name)
+        f = open(SERVER_FOLDER + problem_name + "/problem.json", "w")
+        f.write(problem)
+        f.close()
+        shutil.copytree(folder + RELEASE_FOLDER, SERVER_FOLDER + problem_name + "/static/")
+    except OSError, e:
+        print "Copying release folder...", e
 
+'''
 for problem in problems:
     print problem
+'''
--- problem-init.sh
+++ problem-init.sh
@@ -8,6 +8,6 @@ then
     touch $PROBLEM_NAME/solution.txt
     cp sample-problem.txt $PROBLEM_NAME/problem.txt
     cp sample-grader.py $PROBLEM_NAME/grader.py
-    cp sample-weightmap.json $PROBLEM_NAME/sample-weightmap.json
+    cp sample-weightmap.json $PROBLEM_NAME/weightmap.json
     cp sample-hint.txt $PROBLEM_NAME/hint.txt
 fi
