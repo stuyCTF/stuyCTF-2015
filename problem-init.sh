9dd7655588303a587791fb43e0048a53f1f0ecd1
Danny Qiu <dqiu55@gmail.com>
2015-04-03 16:49:48 -0400

    Move sample files into sample directory

--- problem-init.sh
+++ problem-init.sh
@@ -6,8 +6,8 @@ then
 
     mkdir -p $PROBLEM_NAME/{release,admin}
     touch $PROBLEM_NAME/solution.txt
-    cp sample-problem.txt $PROBLEM_NAME/problem.txt
-    cp sample-grader.py $PROBLEM_NAME/grader.py
-    cp sample-weightmap.json $PROBLEM_NAME/weightmap.json
-    cp sample-hint.txt $PROBLEM_NAME/hint.txt
+    cp sample/sample-problem.txt $PROBLEM_NAME/problem.txt
+    cp sample/sample-grader.py $PROBLEM_NAME/grader.py
+    cp sample/sample-weightmap.json $PROBLEM_NAME/weightmap.json
+    cp sample/sample-hint.txt $PROBLEM_NAME/hint.txt
 fi
