c090d9bb25cef99bb091ab9183d2785102f4cdc8
Danny Qiu <dqiu55@gmail.com>
2015-04-11 04:51:59 -0400

    Update more than meets the eye to unbuffered server

--- More-Than-Meets-The-Eye_60/admin/yoloswag.py
+++ More-Than-Meets-The-Eye_60/admin/yoloswag.py
@@ -1,7 +1,18 @@
 #!/usr/bin/python
+import sys, itertools
 
 flag = "stuyctf{why_backspace_no_werk}"
 
-while True:
-    for c in flag:
-        print c + "\b \b" + "TRANSFORMERS"
+class UnbufferedStream(object):
+    def __init__(self, stream):
+        self.stream = stream
+    def write(self, data):
+        self.stream.write(data)
+        self.stream.flush()
+    def __getattr__(self, attr):
+        return getattr(self.stream, attr)
+
+sys.stdout = UnbufferedStream(sys.stdout)
+
+for c in itertools.cycle(flag):
+    print c + "\b \b" + "TRANSFORMERS"
--- More-Than-Meets-The-Eye_60/solution.py
+++ More-Than-Meets-The-Eye_60/solution.py
@@ -1,7 +1,7 @@
 import socket
 
-HOST = "127.0.0.1"
-PORT = 12345
+HOST = "stuyctf.me"
+PORT = 12347
 
 sock = socket.socket(socket.AF_INET , socket.SOCK_STREAM)
 sock.connect((HOST , PORT))
@@ -10,9 +10,8 @@ flagchars = []
 
 for i in range(0 , 100):
     received = repr(sock.recv(1024))
-    if received == "swag":
-        continue;
-    else:
-        flagchars.append(received)
+    flagchars.append(received)
 
-print flagchars[0].replace(r'\x08 \x08' , "").replace(r'\n' , "").replace("swag", "").split("flag: ")[1]
+print ''.join([flagchar[0] for flagchar in flagchars])
+
+# flag: stuyctf{why_backspace_no_werk}
