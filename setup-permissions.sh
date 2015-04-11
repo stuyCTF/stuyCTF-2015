a6ed3513f09d537a97d11dc1e5506582c985c04e
Chesley Tan <chesleytan97@gmail.com>
2015-04-10 21:43:12 -0400

    Update setup-permissions.sh

--- setup-permissions.sh
+++ setup-permissions.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-# 770 all dirs, including current
+# 770 all dirs, including current (stuyctf)
 PROBLEM_DIRS=$(ls | grep "_[0-9]*\$")
 chmod 770 $PROBLEM_DIRS .
+
+# 770 stuyCTF-Platform
+if [[ -d ../stuyCTF-Platform ]]; then
+    chmod 770 ../stuyCTF-Platform
+fi
+
+# 770 /vagrant
+if [[ -d /vagrant ]]; then
+    sudo chmod 770 /vagrant
+fi
