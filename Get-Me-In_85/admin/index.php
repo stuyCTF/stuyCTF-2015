86c58acb82111a3efde69464108ea7397916f16a
Danny Qiu <dqiu55@gmail.com>
2015-04-15 14:19:43 -0400

    Update php file for Get Me In

--- Get-Me-In_85/admin/index.php
+++ Get-Me-In_85/admin/index.php
@@ -5,20 +5,26 @@
 $flag = "stuyctf{cOOkies_and_hAshing_go_well_together}";
 $secret = "B9S0D2F3";
 
-$username = $_POST["username"]; 
-$password = $_POST["password"];
+$username = (empty($_POST["username"])) ? "admin" : $_POST["username"];
+$username = (empty($_POST["password"])) ? "admin" : $_POST["password"];
 
 if (!empty($_COOKIE["getmein"])) {
-    if ($COOKIE["getmein"] == md5($secret . urldecode($usernamme . $password))) {
-        echo "Congratulations! You are a registered user.\n";
-        die ("The flag is ". $flag);
+    if ($username === "admin" && $password != "admin") {
+        if ($COOKIE["getmein"] == md5($secret . urldecode($username . $password))) {
+            echo "Congratulations! You are a registered user.\n";
+            die ("The flag is ". $flag);
+        }
+        else {
+            die ("Your cookies don't match up! STOP HACKING THIS SITE.");
+        }
     }
     else {
-        die ("Your cookies don't match up! STOP HACKING THIS SITE.");
+        die ("You are not an admin! LEAVE.");
     }
 }
-else {
-    echo "Your credentials don't seem correct...";
+
+if (empty($_COOKIE{"sample-hash"])) {
+    setcookie("sample-hash", md5($secret . urlencode($username . $password)), time() + (60 * 60 * 24 * 7));
 }
 
 if (empty($_COOKIE["source"])) {
@@ -26,35 +32,41 @@ if (empty($_COOKIE["source"])) {
 }
 else {
     if ($_COOKIE["source"] != 0) {
-        echo '
+        echo '<pre>
 $flag = "XXXXXXXXXXXXXXXXXXXXXXX";
 $secret = "XXXXXXXX"; // This secret is 8 characters long for security!
 
-$username = $_POST["username"];
-$password = $_POST["password"];
+$username = (empty($_POST["username"])) ? "admin" : $_POST["username"];
+$username = (empty($_POST["password"])) ? "admin" : $_POST["password"];
 
 if (!empty($_COOKIE["getmein"])) {
-    if ($COOKIE["getmein"] == md5($usernamme . $password . $secret)) {
-        echo "Congratulations! You are a registered user.\n";
-        die ("The flag is ". $flag);
+    if ($username === "admin" && $password != "admin") {
+        if ($COOKIE["getmein"] == md5($secret . urldecode($username . $password))) {
+            echo "Congratulations! You are a registered user.\n";
+            die ("The flag is ". $flag);
+        }
+        else {
+            die ("Your cookies don\'t match up! STOP HACKING THIS SITE.");
+        }
     }
     else {
-        die ("Your cookies don\'t match up! STOP HACKING THIS SITE.");
+        die ("You are not an admin! LEAVE.");
     }
-}
-else {
-    echo "Your credentials don\'t seem correct...";
 }
 
-if (empty!($_COOKIE["source"])) {
+if (empty($_COOKIE{"sample-hash"])) {
+    setcookie("sample-hash", md5($secret . urlencode($username . $password)), time() + (60 * 60 * 24 * 7));
+}
+
+if (empty($_COOKIE["source"])) {
     setcookie("source", 0, time() + (60 * 60 * 24 * 7));
-}   
+}
 else {
     if ($_COOKIE["source"] != 0) {
-        echo PAGE_SOURCE;
+        echo ""; // This source code is outputted here
     }
 }
-    ';
+    </pre>';
     }
 }
 ?>
@@ -64,5 +76,5 @@ else {
 <form method="POST">
     <input type="text" name="username">
     <input type="password" name="password">
-    <button type="submit">
+    <button type="submit">Submit</button>
 </form>
