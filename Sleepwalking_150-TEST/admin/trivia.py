1d8d4261c851f2e052b871797d89990536f3c7fd
Daniel Zabari <zabari.daniel@Gmail.com>
2015-04-16 08:52:25 -0400

    Update trivia.py

--- Sleepwalking_150-TEST/admin/trivia.py
+++ Sleepwalking_150-TEST/admin/trivia.py
@@ -2,15 +2,15 @@ import os
 s=raw_input("Welcome. what's the final answer from my comment?")
 if s.lower()!="cloud atlas":
    print "nah that's wrong"
-   os._exit()
+   os._exit(0)
 print "the perfect four: flac & mp3 320 and mp3 v0 and mp3 v2.\nit's the best kind of merch."
 s=raw_input("the number of donor points squared times 30 plus my year of birth:")
 if s!="2117":
    print "nope"
-   os._exit()
+   os._exit(0)
 print "the flag is near, and so are you,\nso don't be sad, and don't feel blue.\ni know you hate me, don't be a diva,\nthe name of my middle school is:")
 s=raw_input()
 if "barkai" not in s.lower():
    print "cmon..."
-   os._exit()
+   os._exit(0)
 print "damn you good\n"
