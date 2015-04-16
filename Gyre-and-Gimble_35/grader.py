df7dd3fa364e55598e00ea42f676e24d03ca6566
Danny Qiu <dqiu55@gmail.com>
2015-04-16 11:04:21 -0400

    Make grader A LOT more generous because Zabari can't code

--- Gyre-and-Gimble_35/grader.py
+++ Gyre-and-Gimble_35/grader.py
@@ -1,14 +1,21 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 # Python grader file
 # Return True if flag is correct, False otherwise
 # A message is also required to be returned along with the boolean
 #     Custom messages such as `return False, "OOOFFF SO CLOSE"` can be used
 
 flags = [
-"stuyctf{andtovesandthemimsyborogovesmomebewaremyjawsthecatchjubjubshunbandersnatchhisintimefoesobytreeawhileanduffishstoodwithflamethroughwoodasonetwoandvorpalsnickersnackitwithhebackthoujabberwock?mybeamishfrabjouscallayintwasthedidgimblewabewereandraths}",
+"stuyctf{andtovesandthemimsyborogovesmomebewaremyjawsthecatchjubjubshunbandersnatchhisintimefoesobytreeawhileanduffishstoodwithflamethroughwoodasonetwoandvorpalsnickersnackitwithhebackthoujabberwockmybeamishfrabjouscallayintwasthedidgimblewabewereandraths}",
 "stuyctf{andtovesandthemimsyborogovesmomebewaremyjawsthecatchjubjubshunbandersnatchhisintimefoehetumtumstoodthoughtinhejabberwockofwhifflingtulgeyburbledcameonethroughthewentleftandheadgalumphinghasthetomyocalloohchortledjoyandtovesandthemimsyborogovesmome}"
 ]
 
+punctuation = "!;?.,'\"—-"
+
 def grade(arg, key):
+    for punc in punctuation:
+        key = key.replace(punc, "")
     if key in flags:
         return True, "Come to my arms, my beamish boy!"
     else:
